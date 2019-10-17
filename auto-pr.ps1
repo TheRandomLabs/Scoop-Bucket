845af44581f136a9cb9207b63cfdc4fdab0bfe07
TheRandomLabs <therandomlabsinc@gmail.com>
2019-10-17 17:21:39 +1100

    Minor change

--- auto-pr.ps1
+++ auto-pr.ps1
@@ -1,9 +1,13 @@
 # TODO move to Docker container
 
-Set-Location -Path $(Split-Path $MyInvocation.MyCommand.Path)
+$dir = $(Split-Path $MyInvocation.MyCommand.Path)
+
+Set-Location -Path "$dir"
 .\bin\auto-pr.ps1 -p
 
 Set-Location -Path $Env:SCOOP_GLOBAL\buckets\TheRandomLabs
 git fetch --all
 git reset --hard origin/master
 git pull
+
+Set-Location -Path "$dir"
