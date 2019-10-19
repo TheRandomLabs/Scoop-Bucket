ec9c4a423db877751052461a1345e6503b099dcb
TheRandomLabs <therandomlabsinc@gmail.com>
2019-10-19 23:07:25 +1100

    Add Office

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -6,4 +6,5 @@ if (!$env:SCOOP_HOME) {
 
 $auto_pr = "$env:SCOOP_HOME\bin\auto-pr.ps1"
 $dir = "$psscriptroot\..\bucket"
+& "$psscriptroot\reset-office-version.ps1"
 Invoke-Expression -Command "& '$auto_pr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
---
+++ bin/reset-office-version.ps1
@@ -0,0 +1,7 @@
+$dir = "$psscriptroot\..\bucket"
+
+$office = "$dir\office-365-proplus.json"
+(Get-Content \"$office\").Replace(': "office"', ': "0"') | Set-Content -Path \"$office\"
+
+$office = "$dir\office-365-proplus-minimal.json"
+(Get-Content \"$office\").Replace(': "office"', ': "0"') | Set-Content -Path \"$office\"
