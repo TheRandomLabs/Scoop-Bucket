2927ecdb245366f9a26c23809c3e429dd926ba12
TheRandomLabs <therandomlabsinc@gmail.com>
2020-05-04 18:09:01 +1000

    Spotify improvements

--- scripts/blockthespot.ps1
+++ scripts/blockthespot.ps1
@@ -1,25 +1,22 @@
-@cd %~dp0
-@powershell.exe -ExecutionPolicy Bypass -Command "$_=((Get-Content \"%~f0\") -join \"`n\");iex $_.Substring($_.IndexOf(\"goto :\"+\"EOF\")+9)"
-@goto :EOF
-
 $spotify_path = scoop which Spotify
 
 if (-not $spotify_path) {
-    return
+    Write-Error "The spotify-latest` package is not installed."
+    exit 1
 }
 
 $spotify_dir = Split-Path $spotify_path
 
 if ((Split-Path (Split-Path $spotify_dir) -leaf) -ne "spotify-latest") {
     Write-Error "The `spotify-latest` package is not installed."
-    return
+    exit 1
 }
 
 $spotify_running = Get-Process -ErrorAction Ignore -Name Spotify
 Stop-Process -ErrorAction Ignore -Name Spotify | Out-Null
 
 Move-Item -Force "$spotify_dir\chrome_elf.dll" -Destination "$spotify_dir\chrome_elf.dll.original"
-Copy-Item ".\chrome_elf.dll" -Destination "$spotify_dir"
-if (-not (Get-Content -ErrorAction Ignore "$spotify_dir\config.ini")) { Copy-Item ".\config.ini" -Destination "$spotify_dir" }
+Copy-Item "$PSScriptRoot\chrome_elf.dll" -Destination "$spotify_dir"
+if (-not (Get-Content -ErrorAction Ignore "$spotify_dir\config.ini")) { Copy-Item "$PSScriptRoot\config.ini" -Destination "$spotify_dir" }
 
 if ($spotify_running) { Start-Process "$spotify_path" }
