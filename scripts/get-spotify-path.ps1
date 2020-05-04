9cc19aaf4dd948cfb6a7a34917237298c8193564
TheRandomLabs <therandomlabsinc@gmail.com>
2020-05-04 23:58:31 +1000

    Fix Spotify

--- scripts/get-spotify-path.ps1
+++ scripts/get-spotify-path.ps1
@@ -1,5 +1,3 @@
 $spotify_path = scoop which Spotify
-if (-not $spotify_path) { $spotify_path = "$(Split-Path(Split-Path (Split-Path $PSScriptRoot)))\spotify-latest" }
-if (-not $spotify_path) { $spotify_path = "$(Split-Path(Split-Path (Split-Path $PSScriptRoot)))\spotify-with-blockthespot" }
 if (-not $spotify_path) { $spotify_path = "$env:APPDATA\Spotify\Spotify.exe" }
 if (Test-Path $spotify_path) { return $spotify_path }
